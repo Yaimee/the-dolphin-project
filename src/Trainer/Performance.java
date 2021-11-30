707afdd97bc1851cb28f3db8b3686beb04136f1c
Yaimee <rasmusorum@gmail.com>
2021-11-30 18:03:53 +0100

    conflict

--- src/Trainer/Performance.java
+++ src/Trainer/Performance.java
@@ -1,5 +1,13 @@
 package Trainer;
+package Chairman;
 
 public class Performance {
-Member member
+    Member member;
+    double time;
+    Team team;
+    Performance(Member member, double time, Team team) {
+        this.member = member;
+        this.time = time;
+        this.team = team;
+    }
 }
--- src/Trainer/Trainer.java
+++ src/Trainer/Trainer.java
@@ -1,4 +1,24 @@
 package Trainer;
 
 public class Trainer {
+    String username;
+    String password;
+    Trainer (String username, String password) {
+        if(Menu.username == username && Menu.password == password) {
+            this.username = username;
+            this.password = password;
+        }
+    }
+
+    public void displayTopFive() {
+
+    }
+
+    public void createNewCompetition() {
+
+    }
+
+    public void logPerformances() {
+
+    }
 }
